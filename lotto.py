1d7a4cf7bb8648f8eab1fb5b1b8a05dbed4a39c4
rekeshali <rekeshali@gmail.com>
2019-07-18 00:26:09 -0400

    fixed range bug

--- lotto.py
+++ lotto.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 lotto_data_frame = pd.read_csv("http://web.eecs.utk.edu/~cosc505/data/lottonums.csv")
 
 cols = list(lotto_data_frame.columns)
-winn = lotto_data_frame.loc[:,cols[1]].get_values()
+winn = lotto_data_frame.loc[:,cols[1]].get_values() # unfortunately is a list of strings
 mega = lotto_data_frame.loc[:,cols[2]].get_values()
 winn = np.asarray([list(map(int, string.split())) for string in list(winn.flat)])
 
@@ -15,15 +15,15 @@ def freq(data):
     count = []
     dmax = data.max()
     dmin = data.min()
-    for dnum in range(dmin, dmax): # only counts within extrema
+    for dnum in range(dmin, dmax+1): # only counts within extrema
         count.append((data==dnum).sum()) 
     return count
 
 fmega = freq(mega)
-rmega = range(mega.min(), mega.max())
+rmega = range(mega.min(), mega.max()+1)
 
 fwinn = freq(winn)
-rwinn = range(winn.min(), winn.max())
+rwinn = range(winn.min(), winn.max()+1)
 
 plt.plot(rmega, fmega)
 plt.plot(rwinn, fwinn)
